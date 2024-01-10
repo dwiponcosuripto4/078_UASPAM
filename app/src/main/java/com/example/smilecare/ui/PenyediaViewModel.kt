a9054e947f768e8f3502aa7098a36c66d2ac9ae9
Dwiponco Suripto <d.suripto.ft21@mail.umy.ac.id>
2024-01-11 03:06:44 +0700

    implement viewmodel in PenyediaViewModel

--- app/src/main/java/com/example/smilecare/ui/PenyediaViewModel.kt
+++ app/src/main/java/com/example/smilecare/ui/PenyediaViewModel.kt
@@ -6,6 +6,10 @@ import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.smilecare.BookingApplication
+import com.example.smilecare.ui.add.AddViewModel
+import com.example.smilecare.ui.detail.DetailViewModel
+import com.example.smilecare.ui.edit.EditViewModel
+import com.example.smilecare.ui.home.HomeViewModel
 
 fun CreationExtras.aplikasiBooking(): BookingApplication =
     (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as BookingApplication)
@@ -13,6 +17,26 @@ fun CreationExtras.aplikasiBooking(): BookingApplication =
 object PenyediaViewModel {
     val Factory = viewModelFactory {
 
+        initializer {
+            AddViewModel(aplikasiBooking().container.bookingRepository)
+        }
 
+        initializer {
+            HomeViewModel(aplikasiBooking().container.bookingRepository)
+        }
+
+        initializer {
+            DetailViewModel(
+                createSavedStateHandle(),
+                aplikasiBooking().container.bookingRepository
+            )
+        }
+
+        initializer {
+            EditViewModel(
+                createSavedStateHandle(),
+                aplikasiBooking().container.bookingRepository
+            )
+        }
     }
 }
