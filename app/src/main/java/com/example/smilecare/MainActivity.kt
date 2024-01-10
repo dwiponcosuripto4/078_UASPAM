8249696962b9f9ab539fd30b9901d1ec6b974292
Dwiponco Suripto <d.suripto.ft21@mail.umy.ac.id>
2024-01-11 03:17:58 +0700

    implement pengelola halaman in BookingApp

--- app/src/main/java/com/example/smilecare/MainActivity.kt
+++ app/src/main/java/com/example/smilecare/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.smilecare.ui.BookingApp
 import com.example.smilecare.ui.theme.SmileCareTheme
 import com.google.firebase.Firebase
 import com.google.firebase.initialize
@@ -25,7 +26,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    BookingApp()
                 }
             }
         }
--- app/src/main/java/com/example/smilecare/ui/BookingApp.kt
+++ app/src/main/java/com/example/smilecare/ui/BookingApp.kt
@@ -16,6 +16,7 @@ import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
+import com.example.smilecare.navigation.PengelolaHalaman
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -31,8 +32,7 @@ fun BookingApp(
                 .fillMaxSize()
                 .padding(it)
         ) {
-
-
+            PengelolaHalaman()
         }
     }
 }
